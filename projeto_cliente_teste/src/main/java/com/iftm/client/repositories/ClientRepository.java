7c5c5728f59a08270a7f2b45ba03cecff543b63c
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2023-05-04 22:23:07 -0300

    Initial commit

--- projeto_cliente_teste/src/main/java/com/iftm/client/repositories/ClientRepository.java
+++ projeto_cliente_teste/src/main/java/com/iftm/client/repositories/ClientRepository.java
@@ -1,11 +1,58 @@
 package com.iftm.client.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.iftm.client.entities.Client;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    @Query("select c from Client c where upper(c.name) = upper(?1)")
+    Client findByName(String name);
+
+
+    @Query("select c from Client c where upper(c.name) like upper(concat('%', ?1, '%'))")
+    List<Client> findByNameWithContains(String name);
+
+    @Query("select c from Client c where c.income > ?1")
+    List<Client> findBySalaryGreaterThat(Double income);
+
+    @Query("select c from Client c where c.income < ?1")
+    List<Client> findBySalaryLessThat(Double income);
+
+    @Query("select c from Client c where c.income between ?1 and ?2")
+    List<Client> findBySalaryBetween(Double incomeStart, Double incomeEnd);
+
+    @Query("select c from Client c where c.birthDate between ?1 and ?2")
+    List<Client> findByBirthDateBetween(Instant birthDateStart, Instant birthDateEnd);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- projeto_cliente_teste/src/test/java/com/iftm/client/DsclientApplicationTests.java
+++ projeto_cliente_teste/src/test/java/com/iftm/client/DsclientApplicationTests.java
@@ -1,13 +1,168 @@
 package com.iftm.client;
 
+import com.iftm.client.entities.Client;
+import com.iftm.client.repositories.ClientRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class DsclientApplicationTests {
 
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    @DisplayName("Find by name")
+    void findByName() {
+        //Arrange
+        String name = "Jose Saramago";
+        //Act
+        var result = clientRepository.findByName("Jose Saramago");
+        //Assert
+        assertEquals(name, result.getName());
+
+    }
 	@Test
-	void contextLoads() {
+	@DisplayName("Nome inexistente")
+	void findIfExists() {
+		//arrange
+		String nomeInexistente = "Rodolfo Ribeiro";
+		//act
+		var result = clientRepository.findByName(nomeInexistente);
+		//assert
+		assertNull(result);
 	}
+
+    @Test
+    @DisplayName("Texto não existente")
+    void findByNameWithContainsNotExists() {
+        //Arrange
+        String name = "Rodolfo Ribeiro";
+        //Act
+        var result = clientRepository.findByNameWithContains(name);
+        //Assert
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    @DisplayName("Nome Vazio")
+    void findByNameWithContainsEmpty() {
+        //Arrange
+        String name = "";
+        //Act
+        var result = clientRepository.findByNameWithContains(name);
+        //Assert
+        assertEquals(12, result.size());
+    }
+
+    @Test
+    @DisplayName("Find by name with contains")
+    void findByNameWithContains() {
+        //Arrange
+        String name = "ramago";
+        String nomeEsperado = "Jose Saramago";
+        //Act
+        var result = clientRepository.findByNameWithContains(name);
+        //Assert
+        assertEquals(nomeEsperado, result.get(0).getName());
+    }
+
+    @Test
+    @DisplayName("Find by salary greater that")
+    void findBySalaryGreaterThat() {
+        //Arrange
+        int tamanho = 2;
+        Double salary = 5000.0;
+        String nomeEsperado[] = {"Carolina Maria de Jesus", "Toni Morrison"};
+        //Act
+        var result = clientRepository.findBySalaryGreaterThat(salary);
+        //Assert
+        assertEquals(tamanho, result.size());
+        assertEquals(nomeEsperado[0], result.get(0).getName());
+        assertEquals(nomeEsperado[1], result.get(1).getName());
+    }
+
+    @Test
+    @DisplayName("Find by salary less that")
+    void findBySalaryLessThat() {
+        //Arrange
+        Double salary = 2000.0;
+        int tamanho = 3;
+        String nomeEsperado[] = {"Conceição Evaristo", "Yuval Noah Harari", "Chimamanda Adichie"};
+        //Act
+        var result = clientRepository.findBySalaryLessThat(salary);
+        //Assert
+        assertEquals(tamanho, result.size());
+        assertEquals(nomeEsperado[0], result.get(0).getName());
+        assertEquals(nomeEsperado[2], result.get(2).getName());
+    }
+
+    @Test
+    @DisplayName("Find by salary between")
+    void findBySalaryBetween() {
+        //Arrange
+        Double salaryStart = 2000.0;
+        Double salaryEnd = 5000.0;
+        int tamanho = 7;
+        String nomeEsperado[] = {"Lázaro Ramos", "Clarice Lispector", "Gilberto Gil", "Djamila Ribeiro", "Jose Saramago", "Silvio Almeida", "Jorge Amado"};
+        //Act
+        var result = clientRepository.findBySalaryBetween(salaryStart, salaryEnd);
+        //Assert
+        assertEquals(tamanho, result.size());
+        assertEquals(nomeEsperado[0], result.get(0).getName());
+        assertEquals(nomeEsperado[4], result.get(4).getName());
+    }
+
+    @Test
+    @DisplayName("Find by birth date between")
+    void findByBirthDateBetween() {
+        //Arrange
+        Instant data_inicio = Instant.parse("2017-12-25T20:30:50Z");
+        Instant data_final = Instant.now();
+        int quant = 1;
+        String name = "Conceição Evaristo";
+        //Act
+        var result = clientRepository.findByBirthDateBetween(data_inicio, data_final);
+        //Assert
+        assertEquals(quant, result.size());
+        assertEquals(name, result.get(0).getName());
+    }
+
+    @Test
+    @DisplayName("Insert")
+    void insert() {
+        //Arrange
+        String name = "Rodolfo Ribeiro";
+        String cpf = "123456789";
+        Double income = 10000.0;
+        Instant birthDate = Instant.parse("1977-12-25T20:30:50Z");
+        int children = 0;
+        //Act
+        Client clienteTeste = clientRepository.findByName("Jose Saramago");
+        clienteTeste.setName(name);
+        clienteTeste.setCpf(cpf);
+        clienteTeste.setIncome(income);
+        clienteTeste.setBirthDate(birthDate);
+        clienteTeste.setChildren(children);
+        clientRepository.save(clienteTeste);
+        var result = clientRepository.findByName(name);
+        //Assert
+        assertEquals(name, result.getName());
+        assertEquals(cpf, result.getCpf());
+        assertEquals(income, result.getIncome());
+        assertEquals(birthDate, result.getBirthDate());
+        assertEquals(children, result.getChildren());
+    }
 
 }
